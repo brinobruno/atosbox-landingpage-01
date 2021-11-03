2fbd6f78e3aad6d09ec5e20818ac1de1d412fb55
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-11-03 12:55:40 -0300

    section 3 completed

---
+++ assets/js/swiper.js
@@ -0,0 +1,39 @@
+/* const swiper = document.querySelector('.swiper')
+
+swiper = new Swiper ('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+*/
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 4,
+      setWrapperSize: true
+    }
+  }
+})
