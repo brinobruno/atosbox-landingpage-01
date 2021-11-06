50158b797422fb90c00d2e6a3226b2967e328f64
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-11-05 22:09:52 -0300

    section 7 completed

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,18 @@
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '25px',
+    duration: 700,
+    reset: true
+  })
+  
+  scrollReveal.reveal(
+    `.c1 img, .c1 .text,
+    .c2 h3, .c2 .swiper, .c2 .specialist-quote,
+    .c3 .c3-left, .c3 .c3-right,
+    .c4 .c4-title, .c4 .about-benefits, .c4 a,
+    .c5 .c5-title, .c5 .c5-description, .c5 .locations-menu, .c5 .locations-buttons, .c5 .locations-description,
+    .c6 .c6-title, .c6 .c6-description, .c6 .faq-accordion
+    
+    `,
+    { interval: 100 }
+  )
